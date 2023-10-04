2a49b3a801222d3ab6b8f2d4aa0e22c3cf73e067
Igor Putyajkin <putyajkin99@gmail.com>
2023-10-04 17:20:22 +0300

    12.4

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, Integer> basket = new HashMap<>();
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Красный");
+        colors.add("Зеленый");
+        colors.add("Синий");
+        colors.add("Желтый");
 
-        basket.put("Молоко", 20);
-        basket.put("Масло", 10);
-        basket.put("Хлеб", 15);
-
-        TreeMap<String, Integer> basketMap = new TreeMap<>(new ProductComparator());
-
-        basketMap.putAll(basket);
-        for(String product : basketMap.keySet()){
-            System.out.println(product + " - " + basketMap.get(product));
+        Iterator<String> iterator = colors.iterator();
+        while (iterator.hasNext()) {
+            String color = iterator.next();
+            System.out.println(color);
+            if (color.equals("Зеленый")) {
+                iterator.remove();
+            }
         }
-        for(Map.Entry<String, Integer> count : basketMap.entrySet()){
-            System.out.println(count.getKey() + " - " +  count.getValue());
-        }
-        for (Integer count : basketMap.values()) {
-            System.out.println(count);
-        }
+        System.out.println("Итого: " + colors);
 
     }
 }
