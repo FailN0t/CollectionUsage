57daf48b993759d7f7ece29a9ad4a1c4214e1b00
Igor Putyajkin <putyajkin99@gmail.com>
2023-10-05 02:29:22 +0300

    12.5

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,13 @@
-import com.sun.source.tree.Tree;
-
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> colors = new ArrayList<>();
-        colors.add("Красный");
-        colors.add("Зеленый");
-        colors.add("Синий");
-        colors.add("Желтый");
+        Game game = new Game();
+        game.initPlayers();
+        game.add("Андрей Дмитриев");
 
-        Iterator<String> iterator = colors.iterator();
-        while (iterator.hasNext()) {
-            String color = iterator.next();
-            System.out.println(color);
-            if (color.equals("Зеленый")) {
-                iterator.remove();
-            }
-        }
-        System.out.println("Итого: " + colors);
+
 
     }
 }
